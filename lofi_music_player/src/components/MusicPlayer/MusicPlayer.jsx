ca6328271f5ad0699cc9526fc7bcff9a70913287
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-02-01 21:17:22 +0200

    volume control

---
+++ lofi_music_player/src/components/MusicPlayer/MusicPlayer.jsx
@@ -0,0 +1,163 @@
+import { useState, useEffect, useRef, useCallback } from "react";
+import AudioControls from "../AudioControls/AudioControls";
+
+const MusicPlayer = ({ songs }) => {
+  const [songIndex, setSongIndex] = useState(0);
+  const [trackProgress, setTrackProgress] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const { author, path, title } = songs[songIndex];
+  const [volume, setVolume] = useState(0.5);
+
+  const audioRef = useRef(new Audio(path));
+  const intervalRef = useRef();
+  const isReady = useRef(false);
+
+  const { duration } = audioRef.current;
+
+  const toPrevTrack = () => {
+    if (songIndex - 1 < 0) {
+      setSongIndex(songs.length - 1);
+    } else {
+      setSongIndex(songIndex - 1);
+    }
+  };
+
+  const toNextTrack = useCallback(() => {
+    if (songIndex < songs.length - 1) {
+      setSongIndex(songIndex + 1);
+    } else {
+      setSongIndex(0);
+    }
+  }, [songIndex, songs.length]);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    // Pause and clean up on unmount
+    return () => {
+      audioRef.current.pause();
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
+  const startTimer = useCallback(() => {
+    clearInterval(intervalRef.current);
+
+    intervalRef.current = setInterval(() => {
+      if (audioRef.current.ended) {
+        toNextTrack();
+      } else {
+        setTrackProgress(audioRef.current.currentTime);
+      }
+    }, [1000]);
+  }, [toNextTrack]);
+
+  useEffect(() => {
+    audioRef.current.pause();
+    audioRef.current = new Audio(path);
+    setTrackProgress(audioRef.current.currentTime);
+
+    if (isReady.current) {
+      audioRef.current.play();
+      setIsPlaying(true);
+      startTimer();
+    } else {
+      isReady.current = true;
+    }
+  }, [songIndex, path, startTimer]);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+      startTimer();
+    } else {
+      clearInterval(intervalRef.current);
+      audioRef.current.pause();
+    }
+  }, [isPlaying, path, startTimer]);
+
+  useEffect(() => {
+    audioRef.current.pause();
+    audioRef.current = new Audio(path);
+    setTrackProgress(audioRef.current.currentTime);
+
+    if (isReady.current) {
+      audioRef.current.play();
+      setIsPlaying(true);
+      startTimer();
+    }
+  }, [songIndex, path, startTimer]);
+
+  const onScrub = (value) => {
+    clearInterval(intervalRef.current);
+    audioRef.current.currentTime = value;
+    setTrackProgress(audioRef.current.currentTime);
+  };
+
+  useEffect(() => {
+    audioRef.current.volume = volume;
+  }, [volume]);
+
+  const onScrubEnd = () => {
+    if (!isPlaying) {
+      setIsPlaying(true);
+    }
+    startTimer();
+  };
+
+  const handleVolumeChange = (event) => {
+    setVolume(event.target.value);
+    audioRef.current.volume = event.target.value;
+  };
+
+  let s = parseInt(Math.ceil(audioRef.current.currentTime % 60));
+  let m = parseInt((audioRef.current.currentTime / 60) % 60);
+
+  let formattedS = s < 10 ? `0${s}` : `${s}`;
+  let formattedTime = `${m}:${formattedS}`;
+
+  console.log(volume);
+  console.log(`song`, audioRef.current.volume);
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <h3>{author}</h3>
+      <AudioControls
+        isPlaying={isPlaying}
+        onPrevClick={toPrevTrack}
+        onNextClick={toNextTrack}
+        onPlayPauseClick={setIsPlaying}
+      />
+      <div style={{ display: "flex" }}>
+        <input
+          type="range"
+          value={trackProgress}
+          step="1"
+          min="0"
+          max={duration ? duration : `${duration}`}
+          onChange={(e) => onScrub(e.target.value)}
+          onMouseUp={onScrubEnd}
+          onKeyUp={onScrubEnd}
+        />
+        <p>{`${formattedTime}`}</p>
+        <input
+          type="range"
+          min={0}
+          max={1}
+          step={0.01}
+          value={volume}
+          onChange={handleVolumeChange}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MusicPlayer;
