8bea2f0ea7f4c2b22e3112cde525e1315f4691e9
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-02-01 21:17:03 +0200

    added funcionality + trying to make volume control

---
+++ lofi_music_player/src/components/AudioControls/AudioControls.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import {
+  AiFillFastBackward,
+  AiFillFastForward,
+  AiFillPauseCircle,
+  AiFillPlayCircle,
+} from "react-icons/ai";
+
+const AudioControls = ({ isPlaying, onPlayPauseClick, onPrevClick, onNextClick }) => {
+  return (
+    <ControlsContainer>
+      <AiFillFastBackward type="button" aria-label="Previous" onClick={onPrevClick} />
+      {isPlaying ? (
+        <AiFillPauseCircle
+          type="button"
+          aria-label="Pause"
+          onClick={() => onPlayPauseClick(false)}
+        />
+      ) : (
+        <AiFillPlayCircle type="button" aria-label="Pause" onClick={() => onPlayPauseClick(true)} />
+      )}
+
+      <AiFillFastForward type="button" aria-label="Next" onClick={onNextClick} />
+    </ControlsContainer>
+  );
+};
+
+export default AudioControls;
+
+const ControlsContainer = styled.div`
+  font-size: 3em;
+`;
--- lofi_music_player/src/components/SongSwitcher/MusicPlayer.jsx
+++
@@ -1,5 +0,0 @@
-function MusicPlayer() {
-  return <div></div>;
-}
-
-export default MusicPlayer;
