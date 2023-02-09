c04a81d98a1b91e50c0fcf8b4a127b812e423cc6
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-02-09 21:39:44 +0200

    most used colors file

---
+++ lofi_music_player/src/consts/colors.js
@@ -0,0 +1,6 @@
+export const mainBackgroundColor = `background: #ad5389;
+background: -webkit-linear-gradient(to right, #3c1053, #ad5389);
+background: linear-gradient(to right, #3c1053, #ad5389);`;
+
+export const primaryButtonColor = `#673ab7`;
+export const secondaryButtonColor = `#8561c5`;
