a1ddc09ead9b7ccac8e9cf0c144d0bd9199b3ea1
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-02-21 22:08:25 +0200

    home page remade

--- lofi_music_player/src/Pages/Home/Home.jsx
+++ lofi_music_player/src/Pages/Home/Home.jsx
@@ -1,27 +1,68 @@
 import { useState } from "react";
 import { useMusic } from "../../hooks/music";
-import MusicPlayer from "../MusicPlayer/MusicPlayer";
+import MusicPlayer from "../../components/MusicPlayer/MusicPlayer";
+import Buttons from "./Buttons";
+import styled from "styled-components";
+import { mainBackgroundColor } from "../../consts/colors";
 
 const Home = () => {
   const { data } = useMusic();
   const songs = data || [];
-
   const [currentSong, setCurrentSong] = useState(null);
-
   const [started, setStarted] = useState(false);
-
   const start = () => {
     setCurrentSong(songs[0]);
     setStarted(true);
   };
 
-  // return !songs.length ? <p>Loading...</p> : <MusicPlayer songs={songs}></MusicPlayer>;
-
   if (started) {
-    return <div>{currentSong && <MusicPlayer songs={songs}></MusicPlayer>}</div>;
+    return (
+      <Container>
+        {currentSong && (
+          <div style={{}}>
+            <MusicPlayer songs={songs}></MusicPlayer>
+          </div>
+        )}
+      </Container>
+    );
   } else {
-    return <div>{songs.length ? <button onClick={start}>Start</button> : <p>LOADING...</p>}</div>;
+    return (
+      <Container>
+        {songs.length ? (
+          <TextAndButtonContainer>
+            <Buttons onClick={start} />
+            <Footer>
+              <a href="https://github.com/TomasKrin">
+                For more projects, visit my GitHub profile!!
+              </a>
+            </Footer>
+          </TextAndButtonContainer>
+        ) : (
+          <LoadingMessage>LOADING...</LoadingMessage>
+        )}
+      </Container>
+    );
   }
 };
 
 export default Home;
+
+const Container = styled.div`
+  ${mainBackgroundColor}
+  color: white;
+  height: 100vh;
+`;
+
+const TextAndButtonContainer = styled.div`
+  padding-top: 200px;
+`;
+
+const LoadingMessage = styled.p`
+  padding: 20px;
+`;
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+`;
